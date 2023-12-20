ec733ae9a4a130d18481aff31cae370262f67d29
Dorrothee <112761090+Dorrothee@users.noreply.github.com>
2023-12-20 23:00:43 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import spark.Spark;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Spark.port(80);
+
+        Spark.get("/", (req, res) -> "Hello World");
     }
 }
