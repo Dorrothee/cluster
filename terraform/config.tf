0680ab8954ffb601b5f83c452ab65fc719d8e35c
Dorrothee <112761090+Dorrothee@users.noreply.github.com>
2023-12-21 16:15:39 +0200

    Update config.tf

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIAXK2X752ZO6L4AT2S"
-  secret_key = "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"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
  }
